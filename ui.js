9890e43acd06e17d4b1e64bcb29fc473a62c87ee
KhanAuto <ryanhallas@hotmail.com>
2026-08-27 13:19:54 -0700

    Initialize v12 dark theme before UI boot

--- ui.js
+++ ui.js
@@ -2,6 +2,7 @@ document.write(
   '<link rel="stylesheet" href="guided-intake.css">' +
   '<link rel="stylesheet" href="value-readiness.css">' +
   '<link rel="stylesheet" href="experience-v12.css">' +
+  '<script src="theme-v12-pre.js"></script>' +
   '<script src="ui-base.js"></script>' +
   '<script src="guided-intake.js"></script>' +
   '<script src="navigation-v12.js"></script>' +
