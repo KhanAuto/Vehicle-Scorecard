5c87cd79387a93c4ee96569e339f6026b73f9f06
KhanAuto <ryanhallas@hotmail.com>
2026-08-27 13:17:32 -0700

    Load v12 pricing-first garage experience

--- ui.js
+++ ui.js
@@ -1,8 +1,9 @@
 document.write(
   '<link rel="stylesheet" href="guided-intake.css">' +
   '<link rel="stylesheet" href="value-readiness.css">' +
+  '<link rel="stylesheet" href="experience-v12.css">' +
   '<script src="ui-base.js"></script>' +
   '<script src="guided-intake.js"></script>' +
-  '<script src="navigation-fix.js"></script>' +
-  '<script src="value-readiness.js"></script>'
+  '<script src="navigation-v12.js"></script>' +
+  '<script src="experience-v12b.js"></script>'
 );
