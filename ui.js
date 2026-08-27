6778861184c525be33428300ea0f0de91911a9b4
KhanAuto <ryanhallas@hotmail.com>
2026-08-27 02:52:09 -0700

    Load live VIN scanner before guided intake

--- ui.js
+++ ui.js
@@ -1,1 +1,8 @@
-document.write('<link rel="stylesheet" href="guided-intake.css"><script src="ui-base.js"></script><script src="guided-intake.js"></script><script src="guided-intake-hotfix.js"></script>');
+document.write(
+  '<link rel="stylesheet" href="guided-intake.css">' +
+  '<link rel="stylesheet" href="vin-scanner.css">' +
+  '<script src="ui-base.js"></script>' +
+  '<script src="vin-scanner.js"></script>' +
+  '<script src="guided-intake.js"></script>' +
+  '<script src="guided-intake-hotfix.js"></script>'
+);
