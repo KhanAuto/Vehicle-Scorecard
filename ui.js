27af127e601888fe0a700a06eb9342bac09db598
KhanAuto <ryanhallas@hotmail.com>
2026-08-27 04:16:57 -0700

    Load value analysis readiness guidance

--- ui.js
+++ ui.js
@@ -1,6 +1,8 @@
 document.write(
   '<link rel="stylesheet" href="guided-intake.css">' +
+  '<link rel="stylesheet" href="value-readiness.css">' +
   '<script src="ui-base.js"></script>' +
   '<script src="guided-intake.js"></script>' +
-  '<script src="navigation-fix.js"></script>'
+  '<script src="navigation-fix.js"></script>' +
+  '<script src="value-readiness.js"></script>'
 );
