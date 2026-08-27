d9008c26ad78367fc1213b8a9ccc93fda22082ed
KhanAuto <ryanhallas@hotmail.com>
2026-08-27 16:01:16 -0700

    Restore original top navigation and remove bottom dock handlers

--- ui.js
+++ ui.js
@@ -5,14 +5,12 @@ document.write(
   '<link rel="stylesheet" href="garage-polish-v12.css">' +
   '<link rel="stylesheet" href="garage-v12-2.css">' +
   '<link rel="stylesheet" href="garage-expand-v123.css">' +
-  '<link rel="stylesheet" href="drawer-fix-v124.css">' +
+  '<link rel="stylesheet" href="nav-reset-v127.css">' +
   '<script src="theme-v12-pre.js"></script>' +
   '<script src="ui-base.js"></script>' +
   '<script src="guided-intake.js"></script>' +
   '<script src="navigation-v12.js"></script>' +
   '<script src="experience-v12b.js"></script>' +
-  '<script src="garage-polish-v12.js"></script>' +
   '<script src="garage-v12-2.js"></script>' +
-  '<script src="garage-expand-v123.js"></script>' +
-  '<script src="drawer-fix-v124.js"></script>'
+  '<script src="garage-expand-v123.js"></script>'
 );
