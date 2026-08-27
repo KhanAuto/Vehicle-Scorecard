e1918da7d3da67683e2da64bbc4686e54057bcca
KhanAuto <ryanhallas@hotmail.com>
2026-08-27 02:42:57 -0700

    Load guided intake interaction fix

--- ui.js
+++ ui.js
@@ -1,1 +1,1 @@
-document.write('<link rel="stylesheet" href="guided-intake.css"><script src="ui-base.js"></script><script src="guided-intake.js"></script>');
+document.write('<link rel="stylesheet" href="guided-intake.css"><script src="ui-base.js"></script><script src="guided-intake.js"></script><script src="guided-intake-hotfix.js"></script>');
