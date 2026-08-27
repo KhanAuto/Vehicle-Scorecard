a2745ca0d29cf8d3d51a0a8dc5e0baacb98b5ffc
KhanAuto <ryanhallas@hotmail.com>
2026-08-27 03:25:43 -0700

    Remove obsolete VIN scanner runtime from UI loader

--- ui.js
+++ ui.js
@@ -1,8 +1,5 @@
 document.write(
   '<link rel="stylesheet" href="guided-intake.css">' +
-  '<link rel="stylesheet" href="vin-scanner.css">' +
   '<script src="ui-base.js"></script>' +
-  '<script src="vin-scanner.js"></script>' +
-  '<script src="guided-intake.js"></script>' +
-  '<script src="guided-intake-hotfix.js"></script>'
+  '<script src="guided-intake.js"></script>'
 );
