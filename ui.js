8f78d916df2c65d8d34ed41f733b093315dbf72b
KhanAuto <ryanhallas@hotmail.com>
2026-08-27 13:27:40 -0700

    Load v12.1 garage polish and bottom navigation

--- ui.js
+++ ui.js
@@ -2,9 +2,11 @@ document.write(
   '<link rel="stylesheet" href="guided-intake.css">' +
   '<link rel="stylesheet" href="value-readiness.css">' +
   '<link rel="stylesheet" href="experience-v12.css">' +
+  '<link rel="stylesheet" href="garage-polish-v12.css">' +
   '<script src="theme-v12-pre.js"></script>' +
   '<script src="ui-base.js"></script>' +
   '<script src="guided-intake.js"></script>' +
   '<script src="navigation-v12.js"></script>' +
-  '<script src="experience-v12b.js"></script>'
+  '<script src="experience-v12b.js"></script>' +
+  '<script src="garage-polish-v12.js"></script>'
 );
