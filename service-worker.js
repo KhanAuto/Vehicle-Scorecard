8d1b7c8123bbea9abc6508a8640e2654601ea268
KhanAuto <ryanhallas@hotmail.com>
2026-08-27 23:10:55 -0700

    Refresh app shell for linked readiness

--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = "vehicle-scorecard-app-shell-v12-40";
+const CACHE_NAME = "vehicle-scorecard-app-shell-v12-41";
 
 const CORE_ASSETS = [
   "./", "./index.html", "./styles.css", "./core.js", "./inspection.js", "./intelligence.js", "./ui.js",
