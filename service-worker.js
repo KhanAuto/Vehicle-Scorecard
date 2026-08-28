337b9b677828e310f0a675c5101bc19e17832b8b
KhanAuto <ryanhallas@hotmail.com>
2026-08-27 21:40:57 -0700

    Refresh app shell for compact progress label

--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = "vehicle-scorecard-app-shell-v12-30";
+const CACHE_NAME = "vehicle-scorecard-app-shell-v12-31";
 
 const CORE_ASSETS = [
   "./", "./index.html", "./styles.css", "./core.js", "./inspection.js", "./intelligence.js", "./ui.js",
