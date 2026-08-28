f95459c1ca359e8b1c29021066c20674451bc9c0
KhanAuto <ryanhallas@hotmail.com>
2026-08-27 19:36:53 -0700

    Refresh app shell for adaptive inspection sections

--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = "vehicle-scorecard-app-shell-v12-18";
+const CACHE_NAME = "vehicle-scorecard-app-shell-v12-19";
 
 const CORE_ASSETS = [
   "./", "./index.html", "./styles.css", "./core.js", "./inspection.js", "./intelligence.js", "./ui.js",
