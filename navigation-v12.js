2bcb795008cf1015594dd6d3f282a6fd0c96b824
KhanAuto <ryanhallas@hotmail.com>
2026-08-27 23:02:56 -0700

    Update market completion for exit offers

--- navigation-v12.js
+++ navigation-v12.js
@@ -57,7 +57,7 @@
       return Boolean(s?.answered);
     }
     if (pageId === "marketPage") {
-      return ["kbbTrade","kbbPrivate","edmundsTrade","edmundsPrivate","private1Price","private2Price","private3Price","dealer1Price","dealer2Price","dealer3Price","instantOffer"]
+      return ["kbbTrade","kbbPrivate","edmundsTrade","edmundsPrivate","estimatedWholesale","private1Price","private2Price","private3Price","dealer1Price","dealer2Price","dealer3Price","instantOffer","instantOffer2","dealerCashOffer","actualTradeOffer"]
         .some((id) => APP.numberFrom?.(id) > 0);
     }
     if (pageId === "dealPage") {
