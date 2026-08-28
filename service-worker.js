22690b3263c65182789734e5b5ceeca4922d65ed
KhanAuto <ryanhallas@hotmail.com>
2026-08-27 22:29:22 -0700

    Refresh app shell for selling requirements

--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = "vehicle-scorecard-app-shell-v12-37";
+const CACHE_NAME = "vehicle-scorecard-app-shell-v12-38";
 
 const CORE_ASSETS = [
   "./", "./index.html", "./styles.css", "./core.js", "./inspection.js", "./intelligence.js", "./ui.js",
