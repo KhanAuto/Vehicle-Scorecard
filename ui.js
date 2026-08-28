f31fa3e7d12133391e2617636f2f1811475ca1e6
KhanAuto <ryanhallas@hotmail.com>
2026-08-27 22:49:23 -0700

    Version UI assets for badge fix

--- ui.js
+++ ui.js
@@ -1,20 +1,20 @@
 document.write(
-  '<link rel="stylesheet" href="guided-intake.css?v=12.38">' +
-  '<link rel="stylesheet" href="value-readiness.css?v=12.38">' +
-  '<link rel="stylesheet" href="experience-v12.css?v=12.38">' +
-  '<link rel="stylesheet" href="garage-polish-v12.css?v=12.38">' +
-  '<link rel="stylesheet" href="garage-v12-2.css?v=12.38">' +
-  '<link rel="stylesheet" href="garage-expand-v123.css?v=12.38">' +
-  '<link rel="stylesheet" href="new-report-v122.css?v=12.38">' +
-  '<link rel="stylesheet" href="garage-management-v127.css?v=12.38">' +
-  '<link rel="stylesheet" href="nav-reset-v127.css?v=12.38">' +
-  '<link rel="stylesheet" href="navigation-shell-v1210.css?v=12.38">' +
-  '<script src="theme-v12-pre.js?v=12.38"></script>' +
-  '<script src="ui-base.js?v=12.38"></script>' +
-  '<script src="guided-intake.js?v=12.38"></script>' +
-  '<script src="navigation-v12.js?v=12.38"></script>' +
-  '<script src="experience-v12b.js?v=12.38"></script>' +
-  '<script src="garage-v12-2.js?v=12.38"></script>' +
-  '<script src="garage-expand-v123.js?v=12.38"></script>' +
-  '<script src="navigation-shell-v1210.js?v=12.38"></script>'
+  '<link rel="stylesheet" href="guided-intake.css?v=12.39">' +
+  '<link rel="stylesheet" href="value-readiness.css?v=12.39">' +
+  '<link rel="stylesheet" href="experience-v12.css?v=12.39">' +
+  '<link rel="stylesheet" href="garage-polish-v12.css?v=12.39">' +
+  '<link rel="stylesheet" href="garage-v12-2.css?v=12.39">' +
+  '<link rel="stylesheet" href="garage-expand-v123.css?v=12.39">' +
+  '<link rel="stylesheet" href="new-report-v122.css?v=12.39">' +
+  '<link rel="stylesheet" href="garage-management-v127.css?v=12.39">' +
+  '<link rel="stylesheet" href="nav-reset-v127.css?v=12.39">' +
+  '<link rel="stylesheet" href="navigation-shell-v1210.css?v=12.39">' +
+  '<script src="theme-v12-pre.js?v=12.39"></script>' +
+  '<script src="ui-base.js?v=12.39"></script>' +
+  '<script src="guided-intake.js?v=12.39"></script>' +
+  '<script src="navigation-v12.js?v=12.39"></script>' +
+  '<script src="experience-v12b.js?v=12.39"></script>' +
+  '<script src="garage-v12-2.js?v=12.39"></script>' +
+  '<script src="garage-expand-v123.js?v=12.39"></script>' +
+  '<script src="navigation-shell-v1210.js?v=12.39"></script>'
 );
